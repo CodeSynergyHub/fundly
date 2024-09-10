3d2cc55eabafcd3902a4d3f999054509cdcf0c79
Mohsen Bagheri <mohsenbaqeri10@gmail.com>
2024-09-10 14:34:59 +0330

    feat(campaign): additional value check on campaign goal_amount field

--- services/campaign/campaign/serializers/campaign.py
+++ services/campaign/campaign/serializers/campaign.py
@@ -8,6 +8,19 @@ class CampaignWriteSerializer(serializers.ModelSerializer):
         fields = ['title', 'description', 'goal_amount', 'start_date', 'end_date',
                   'creator_id']
 
+    def validate_goal_amount(self, value):
+        """
+        Ensure that the goal_amount is positive.
+        """
+        if value < 0:
+            raise serializers.ValidationError("Goal amount must be a positive value.")
+        return value
+
+    def create(self, validated_data):
+        if validated_data.get("goal_amount") < 0:
+            raise serializers.ValidationError({"goal_amount": "Goal amount must be a positive value."})
+        return super().create(validated_data)
+
 
 class CampaignDetailSerializer(serializers.ModelSerializer):
     class Meta:
